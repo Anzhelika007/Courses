f2cd1c10c5a2d97ac203fa9999b02370659052a2
Anzhalika Turlak <293852647@mail.ru>
2024-04-24 22:56:17 +0300

    feat: use class

--- chapter-6-React-Base/my-app/src/App.js
+++ chapter-6-React-Base/my-app/src/App.js
@@ -1,3 +1,4 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,22 +6,36 @@ const Header = () => {
   return <h2>Hello world!</h2>
 }
 
-const Field = () => {
-  const holder = 'Enter here';
-  const styledField = {
-    width: '300px'
-  }
-  return <input 
-          placeholder={holder} 
-          type='text' 
-          style={styledField}/>
-}
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   }
+//   return <input 
+//           placeholder={holder} 
+//           type='text' 
+//           style={styledField}/>
+// }
 
 function Btn() {
   const text = 'Log in';
   const logged = false;
 
   return <button>{logged ? 'Enter' : text}</button>
+}
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input 
+            placeholder={holder} 
+            type='text' 
+            style={styledField}/>
+  }
 }
 
 
@@ -34,4 +49,5 @@ function App() {
   );
 }
 
+export {Header};
 export default App;
