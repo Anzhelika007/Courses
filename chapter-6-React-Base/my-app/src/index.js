2840ca5002e44a14a45c6c672e33a4047cb2b34d
Anzhalika Turlak <293852647@mail.ru>
2024-04-24 22:07:10 +0300

    feat: create element stract

--- chapter-6-React-Base/my-app/src/index.js
+++ chapter-6-React-Base/my-app/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-//const elem = <h2>Hello world!</h2>;
+//const elem = <h2>Hello world!</h2>; // babel
 
 const elem = React.createElement('h2', {className: 'greeting'}, 'Hello world!');
+
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Привет, мир!'
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
