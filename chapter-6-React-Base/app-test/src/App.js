c288edac3c4e94fab44a9aa8384acd25ae07fb6f
Anzhalika Turlak <293852647@mail.ru>
2024-04-25 20:47:17 +0300

    feat: add bind

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -6,23 +6,41 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 27,
-      text: '+++'
+      text: '+++',
+      position: ''
     }
+    this.nextYear = this.nextYear.bind(this);
   }
 
-  nextYear = () => {
+  nextYear() {
     this.setState(state => ({
         years: state.years + 1
     }))
   }
 
+  commitInputChnges = (e) => {
+    /*console.log(e.target.value)*/
+    this.setState({
+      position: e.target.value
+    })
+  }
+
   render() {
     const{name, surname, link} = this.props;
+    const {position, years} = this.state;
+
+    console.log(this)
     return (
       <div>
         <button onClick={this.nextYear} >{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+        <h1>My name is {name}, surname - {surname}, 
+            age - {years}, 
+            position - {position} </h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInputChnges}/>
+        </form>
       </div>
     )
   }
