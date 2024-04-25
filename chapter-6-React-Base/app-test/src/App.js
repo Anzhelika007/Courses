ad13fa555302c5fedbfb3d81cd97c61c2238b25e
Anzhalika Turlak <293852647@mail.ru>
2024-04-25 20:53:55 +0300

    feat: add anon arrow func

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -11,14 +11,15 @@ class WhoAmI extends Component {
     }
   }
 
-  nextYear() {
+  nextYear = () => {
     this.setState(state => ({
         years: state.years + 1
     }))
   }
 
-  commitInputChnges = (e) => {
+  commitInputChnges = (e, color) => {
     /*console.log(e.target.value)*/
+    console.log(color)
     this.setState({
       position: e.target.value
     })
@@ -28,17 +29,16 @@ class WhoAmI extends Component {
     const{name, surname, link} = this.props;
     const {position, years} = this.state;
 
-    console.log(this)
     return (
       <div>
-        <button onClick={() =>this.nextYear()} >{this.state.text}</button>
+        <button onClick={this.nextYear} >{this.state.text}</button>
         <h1>My name is {name}, surname - {surname}, 
             age - {years}, 
             position - {position} </h1>
         <a href={link}>My profile</a>
         <form>
           <span>Введите должность</span>
-          <input type="text" onChange={this.commitInputChnges}/>
+          <input type="text" onChange={(e) => this.commitInputChnges(e, 'someColor')}/>
         </form>
       </div>
     )
