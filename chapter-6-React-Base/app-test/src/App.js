f40a50b979f1c9f99db61d865aac9b23ce3dc4b1
Anzhalika Turlak <293852647@mail.ru>
2024-04-25 17:48:04 +0300

    feat: add state

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -1,18 +1,38 @@
+import { Component } from 'react';
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27
+    }
+  }
+
+  nextYear = () => {
+    console.log('+++');
+    this.setState(state => ({
+        years: state.years + 1
+    }))
+  }
+
+  render() {
+    const{name, surname, link} = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear} >+++</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return 'Alex'} } surname="Sheldon" link='vk.com' />
+      <WhoAmI name='John' surname="Smith" link='facebook.com' />
+      <WhoAmI name='Alex' surname="Sheldon" link='vk.com' />
     </div>
   );
 }
