d0e4225a2a92cce175ca21a32054a469cd5e75bc
Anzhalika Turlak <293852647@mail.ru>
2024-04-25 16:23:09 +0300

    feat: add props

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function WhoAmI ({name, surname, link}) {
   return (
     <div>
-      <h1>My name is {name}, surname - {surname}</h1>
+      <h1>My name is {name.firstName}, surname - {surname}</h1>
       <a href={link}>My profile</a>
     </div>
   )
@@ -14,6 +14,8 @@ function App() {
     <div className="App">
       <WhoAmI name='John' surname="Smith" link='facebook.com' />
       <WhoAmI name='Alex' surname="Sheldon" link='vk.com' />
+      <WhoAmI name={{firstName: 'Alex'}} surname="Sheldon" link='vk.com' />
+      <WhoAmI name={() => {return 'Alex'} } surname="Sheldon" link='vk.com' />
     </div>
   );
 }
