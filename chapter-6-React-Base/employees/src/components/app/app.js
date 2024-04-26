65ae9aae1e68dfd41d98df6dc2a3f9a8ccf181e1
Anzhalika Turlak <293852647@mail.ru>
2024-04-26 13:45:30 +0300

    feat: add methods for increase employees and delete

--- chapter-6-React-Base/employees/src/components/app/app.js
+++ chapter-6-React-Base/employees/src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import './app.css';
 import '../search-panel/search-panel.css';
 import SearchPanel from '../search-panel/search-panel';
@@ -6,27 +7,76 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 15000, increase: false, id: 3},
+      ]
+    }
+    this.maxId = 4;
+  }
 
-  const data = [
-    {name: 'John C.', salary: 800, increase: false, id: 1},
-    {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-    {name: 'Carl W.', salary: 15000, increase: false, id: 3},
-  ];
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      //const index = data.findIndex(elem => elem.id === id);
+      //const before = data.slice(0, index);
+      //const after = data.slice(index+1);
+      //const newArr = [...before, ...after];
 
-  return (
-    <div className="app">
-      <AppInfo />
+      return {
+        data: data.filter(item => item.id !== id)
+      }
 
-      <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+        name, 
+        salary,
+        increase: false,
+        id: this.maxId++
+    }
+    this.setState(({data}) => {
+        const newArr = [...data, newItem];
+        return {
+            data: newArr
+        }
+    });
+  }
+
+  onToggleIncrease = (id) => {
+    console.log(`Increase this ${id}`)
+  }
+
+  onToggleRise = (id) => {
+    console.log(`Rise this ${id}`)
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+  
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+  
+        <EmployeesList 
+        data = {this.state.data}
+        onDelete={this.deleteItem}
+        onToggleIncrease = {this.onToggleIncrease}
+        onToggleRise = {this.onToggleRise}/>
+        <EmployeesAddForm onAdd={this.addItem}/>
       </div>
-
-      <EmployeesList data = {data}/>
-      <EmployeesAddForm/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- chapter-6-React-Base/employees/src/components/employees-list-item/employees-list-item.js
+++ chapter-6-React-Base/employees/src/components/employees-list-item/employees-list-item.js
@@ -21,10 +21,10 @@ class EmployeesListItem extends Component {
     this.setState(({rise}) => ({
         rise: !rise
     }))
-}
+  }
 
   render() {
-    const {name, salary} = this.props;
+    const {name, salary, onDelete, onToggleIncrease, onToggleRise} = this.props;
     const {increase, rise} = this.state;
 
     let classNames = "list-group-item d-flex justify-content-between";
@@ -40,17 +40,18 @@ class EmployeesListItem extends Component {
 
     return (
       <li className={classNames} >
-        <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+        <span className="list-group-item-label" onClick={onToggleRise}>{name}</span>
         <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
         <div className='d-flex justify-content-center align-items-center'>
           <button type="button"
               className="btn-cookie btn-sm "
-              onClick={this.onIncrease}>
+              onClick={onToggleIncrease}>
               <i className="fas fa-cookie"></i>
           </button>
 
           <button type="button"
-                  className="btn-trash btn-sm ">
+                  className="btn-trash btn-sm " 
+                  onClick={onDelete}>
               <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
