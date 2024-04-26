2c472b246cd0cbd42d1641874db30f41b24cdff1
Anzhalika Turlak <293852647@mail.ru>
2024-04-26 17:56:17 +0300

    feat: add filter

--- chapter-6-React-Base/employees/src/components/search-panel/search-panel.js
+++ chapter-6-React-Base/employees/src/components/search-panel/search-panel.js
@@ -1,13 +1,31 @@
+import { Component } from "react";
 
 
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    }
 
-const SearchPanel = () => {
-  return (
-    <input 
-      className="form-control search-input" 
-      type="text" 
-      placeholder="Найти сотрудника"/>
-  )
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onUpdateSearch(term); // здесь метод из компонента App!!!
+  }
+
+  render() {
+    return (
+      <input 
+        className="form-control search-input" 
+        type="text" 
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}/>
+    )
+  }
 }
 
 export default SearchPanel;
