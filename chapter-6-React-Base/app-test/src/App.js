876da043c6780723cb2ec82a18df9974c11a627a
Anzhalika Turlak <293852647@mail.ru>
2024-04-26 16:56:21 +0300

    feat: add fragment

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import './App.css';
 
 class WhoAmI extends Component {
@@ -30,7 +30,7 @@ class WhoAmI extends Component {
     const {position, years} = this.state;
 
     return (
-      <div>
+      <Fragment>
         <button onClick={this.nextYear} >{this.state.text}</button>
         <h1>My name is {name}, surname - {surname}, 
             age - {years}, 
@@ -40,7 +40,7 @@ class WhoAmI extends Component {
           <span>Введите должность</span>
           <input type="text" onChange={(e) => this.commitInputChnges(e, 'someColor')}/>
         </form>
-      </div>
+      </Fragment>
     )
   }
 }
