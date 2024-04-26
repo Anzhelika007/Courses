381401969cfa777633467727ea5738c954bfafbe
Anzhalika Turlak <293852647@mail.ru>
2024-04-26 16:48:58 +0300

    feat: add info

--- chapter-6-React-Base/employees/src/components/employees-list/employees-list.js
+++ chapter-6-React-Base/employees/src/components/employees-list/employees-list.js
@@ -3,7 +3,7 @@ import './employees-list.css';
 
 
 
-const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleRise}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
   const elements = data.map(item => {
     const {id, ...itemProps} = item;
@@ -11,8 +11,7 @@ const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleRise}) => {
       <EmployeesListItem key={id} 
       {...itemProps}
       onDelete={() => onDelete(id)}
-      onToggleIncrease={() => onToggleIncrease(id)}
-      onToggleRise={() => onToggleRise(id)}/>
+      onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
     )
   })
 
