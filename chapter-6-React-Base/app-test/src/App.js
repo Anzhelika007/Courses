4e00da3d687c833b3986f6d00c55c643e65277e9
Anzhalika Turlak <293852647@mail.ru>
2024-04-26 22:37:11 +0300

    feat: add styled-components

--- chapter-6-React-Base/app-test/src/App.js
+++ chapter-6-React-Base/app-test/src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
 class WhoAmI extends Component {
@@ -45,13 +47,17 @@ class WhoAmI extends Component {
   }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper className="App">
       <WhoAmI name='John' surname="Smith" link='facebook.com' />
       <WhoAmI name='Alex' surname="Sheldon" link='vk.com' />
-    </div>
+    </Wrapper>
   );
 }
 
