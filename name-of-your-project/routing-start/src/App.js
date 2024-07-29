24e755750156753ae48aa1d086491687348cf992
Anzhalika Turlak <293852647@mail.ru>
2024-07-29 15:25:28 +0400

    feat: implement page notFound

--- name-of-your-project/routing-start/src/App.js
+++ name-of-your-project/routing-start/src/App.js
@@ -8,6 +8,7 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,6 +20,8 @@ const router = createBrowserRouter(
           <Route path='faq' element={<Faq />} />
           <Route path='contact' element={ <Contact /> }/>
         </Route>
+
+        <Route path='*' element={<NotFound />} />
     </Route>
   )
 )
