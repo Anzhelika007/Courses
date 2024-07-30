95980db6084c31e1844f3eae04a2cb573335cb7c
Anzhelika Turlak <70901455+Anzhelika007@users.noreply.github.com>
2024-07-30 14:18:39 +0300

    Routing 6.4 (#16)
    * feat: add files project
    
    * feat: add routing basics
    
    * feat: add createBrowserRouter
    
    * Routing 6.4 navigate component (#9)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * feat: add params for url
    
    * feat: add errorElement for fetch
    
    * feat: add breadcrumbs
    
    * feat: add form and action send and checking value
    
    * feat: add Navigate Component
    
    * fix: change naming folder
    
    * fix: change naming folder
    
    * Routing 6.4 breadcrumbs (#10)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * feat: add params for url
    
    * feat: add errorElement for fetch
    
    * feat: add breadcrumbs
    
    * docs: add template for Pull Request
    
    * Routing 6.4 forms actions (#11)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * feat: add params for url
    
    * feat: add errorElement for fetch
    
    * feat: add breadcrumbs
    
    * feat: add form and action send and checking value
    
    * fix: change naming folder
    
    * Routing 6.4 error element (#12)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * feat: add params for url
    
    * feat: add errorElement for fetch
    
    * fix: change naming folder
    
    * Routing 6.4 parameters (#13)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * feat: add params for url
    
    * fix: change naming folder
    
    * Routing 6.4 loaders (#14)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * feat: add loader server-json for data
    
    * fix: change naming folder
    
    * Routing 6.4 page error (#15)
    
    * feat: add some nested elements for routing
    
    * feat: implement page notFound
    
    * fix: change naming folder

---
+++ 7-routing-parameters/src/pages/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+    </div>
+  )
+}
---
+++ 7-routing-parameters/src/pages/Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima itaque pariatur, id fuga, fugiat necessitatibus quis explicabo deleniti ad aperiam vitae repellendus quo tempore molestias quae, in assumenda mollitia voluptates!</p>
+    </div>
+  )
+}
