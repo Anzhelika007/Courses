dfa5e924e0e65683ffb72df1e49941270d11e989
Anzhalika Turlak <293852647@mail.ru>
2024-08-05 11:51:56 +0400

    feat: add route

---
+++ 2-app-routes/src/components/App.js
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Categories from "./Categories";
+import Category from "./Category";
+import About from "./About";
+import  Confirmation from "./Confirmation";
+import Header from "./Header";
+import Register from "./Register";
+import Session from "./Session";
+
+
+
+import logo from "../assets/red30-tech-logo.png";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="container">
+        <img
+          className="logo"
+          src={logo}
+          alt="Red30 Tech logo"
+          title="Red30 Tech | Home"
+        />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech"/>} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+
+      <footer className="container">
+        &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
